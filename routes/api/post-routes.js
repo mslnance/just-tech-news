c3833cd15dbcf49087da140176736af7668b197a
Symphony <55611595+lnance6@users.noreply.github.com>
2020-11-13 21:37:07 -0800

    vote on posts

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Vote, Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
       // Query configuration
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      // update the `.findAll()` method's attributes to look like this
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+  ],
       order: [['created_at', 'DESC']], 
       include: [
         {
@@ -23,12 +31,18 @@ router.get('/', (req, res) => {
   
   });
 
-router.get('/:id', (req, res) => {
+  router.get('/:id', (req, res) => {
     Post.findOne({
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
         {
           model: User,
@@ -47,7 +61,7 @@ router.get('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-});
+  });
 
 router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
@@ -61,6 +75,17 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
@@ -105,5 +130,7 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
       });
 });
+
+
 
 module.exports = router;
